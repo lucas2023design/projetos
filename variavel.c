cea09ebeff871b9461b91fb9eac67b9c0d6f9b9a
lucas2023design <142102012+lucas2023design@users.noreply.github.com>
2024-08-13 21:14:33 -0300

    Add files via upload

---
+++ variavel.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+int main() {
+   
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, resto;
+    float divisao;
+
+    numero1 = 20;
+    numero2 = 5;
+
+    //operações 
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float)numero1 / numero2;
+    resto = numero1 % numero2;
+
+    //resultados
+    printf("Numero 1: %d\n", numero1);
+    printf("Numero 2: %d\n", numero2);
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Divisao: %.2f\n", divisao);  // Duas casas decimais
+    printf("Multiplicacao: %d\n", multiplicacao);
+    printf("Resto: %d\n\n", resto);
+
+    
+    numero1 = -20;
+    numero2 = 5;
+
+    //operações
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float)numero1 / numero2;
+    resto = numero1 % numero2;
+
+    // resultado
+    printf("Numero 1: %d\n", numero1);
+    printf("Numero 2: %d\n", numero2);
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Divisao: %.2f\n", divisao);
+    printf("Multiplicacao: %d\n", multiplicacao);
+    printf("Resto: %d\n\n", resto);
+
+   
+    numero1 = -30;
+    numero2 = 4;
+
+    //operações
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float)numero1 / numero2;
+    resto = numero1 % numero2;
+
+    //resultado
+    printf("Numero 1: %d\n", numero1);
+    printf("Numero 2: %d\n", numero2);
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Divisao: %.2f\n", divisao);
+    printf("Multiplicacao: %d\n", multiplicacao);
+    printf("Resto: %d\n\n", resto);
+
+   
+    numero1 = -40;
+    numero2 = 3;
+
+    //operações
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float)numero1 / numero2;
+    resto = numero1 % numero2;
+
+    //resultado
+    printf("Numero 1: %d\n", numero1);
+    printf("Numero 2: %d\n", numero2);
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Divisao: %.2f\n", divisao);
+    printf("Multiplicacao: %d\n", multiplicacao);
+    printf("Resto: %d\n\n", resto);
+
+    
+    numero1 = -50;
+    numero2 = 2;
+
+    //operações
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float)numero1 / numero2;
+    resto = numero1 % numero2;
+
+    //resultados
+    printf("Numero 1: %d\n", numero1);
+    printf("Numero 2: %d\n", numero2);
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Divisao: %.2f\n", divisao);
+    printf("Multiplicacao: %d\n", multiplicacao);
+    printf("Resto: %d\n\n", resto);
+
+ 
+    numero1 = -60;
+    numero2 = 1;
+
+    //operações 
+    soma = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = (float)numero1 / numero2;
+    resto = numero1 % numero2;
+
+    //resultados
+    printf("Numero 1: %d\n", numero1);
+    printf("Numero 2: %d\n", numero2);
+    printf("Soma: %d\n", soma);
+    printf("Subtracao: %d\n", subtracao);
+    printf("Divisao: %.2f\n", divisao);
+    printf("Multiplicacao: %d\n", multiplicacao);
+    printf("Resto: %d\n\n", resto);
+
+}
+
